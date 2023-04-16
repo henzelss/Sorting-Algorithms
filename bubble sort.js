f472e1d96b860ce4e93f9c5526b3adc17888cdc6
Henzel Quitaneg <110615201+henzelss@users.noreply.github.com>
2023-04-16 11:22:34 +0800

    Update bubble sort.js

--- bubble sort.js
+++ bubble sort.js
@@ -2,20 +2,19 @@
     ASC BUBBLE SORT BY HENZEL
  */
 
-//sample array that was declared
-var sampleArray = [2, 4, 232, 43, 5, 7, 34, 43, 23245, 7, 8];
+function bubbleSort (array) {
 
-//whole array iteration
-for (let i = 0; i < sampleArray.length - 1; i++) {
-    //iteration for comparing each element in the array
-    for (let j = 0; j < sampleArray.length - 1 - i; j++) {
-        let placeholder;
-        //actual logic for swapping each element in an array
-        if (sampleArray[j] > sampleArray[j+1]) {
-            placeholder = sampleArray [j];
-            sampleArray[j] = sampleArray [j+1];
-            sampleArray[j+1] = placeholder;
+    for (let i = 0; i < array.length; i++) {
+        
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            if (array[j] > array[j+1]){
+                [array[j], array[j+1]] = [array[j+1], array[j]];
+                
+            }
+            
         }
+        
     }
-    console.log('The arrays are ' +sampleArray);
+    return array;
 }
+console.log(bubbleSort([3,4,342,2,3,2,5,6,7,8,8,9,234]));

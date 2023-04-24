11c675695b80da4da538339a20b4eb4ff766762f
Henzel Quitaneg <110615201+henzelss@users.noreply.github.com>
2023-04-24 18:01:30 +0800

    Update quick sort.js

--- quick sort.js
+++ quick sort.js
@@ -1,18 +1,24 @@
 
 
 function quickSort(arr) {
-    if (arr.length <= 1) {
+
+    if (arr.length <= 1){
         return arr;
     }
 
     const pivot = arr[arr.length - 1];
-    const left = [];
-    const right = [];
+    const leffArr = []
+    const rigthArr = []
 
-    for (const el of arr.slice(0, arr.length -1)){
-        el < pivot ? left.push(el) : right.push(el);
+    for (const el of arr.slice(0, arr.length - 1)) {
+        el < pivot ? leffArr.push(el) : rigthArr.push(el);
     }
-    return [... quickSort(left), pivot, ...quickSort(right)];
+    return [... quickSort(leffArr), pivot, ... quickSort(rigthArr)] 
+
+
+
+
+
 }
 
 const arr = [2,3,4,4,3,6,678,79,54,34,123,6546,79,4534,678679,7989,654];
